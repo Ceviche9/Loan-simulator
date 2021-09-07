94c768176808344ed6d3deef93e189fbb8be299d
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-09-07 20:31:44 -0300

    ssl

--- Backend/dist/config/database.js
+++ Backend/dist/config/database.js
@@ -4,14 +4,20 @@
 
 module.exports = {
   url: process.env.DATABASE_URL,
-  dialect: 'postgres',
-  host: "localhost",
-  username: "postgres",
-  password: "docker",
-  database: "Letalk",
+  dialect: "postgres",
+  host: "ec2-107-20-24-247.compute-1.amazonaws.com",
+  username: "ghzxinjzpphaui",
+  password: "218b80d1047e3827eed995aefeb5e09e31baf8c220b28313f1ae5de18b75bd6c",
+  database: "db1dicruq3hqqe",
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
   define: {
     timestamps: true,
     underscored: true,
-    underscoredAll: true,
+    underscoredAll: true
   },
 };
