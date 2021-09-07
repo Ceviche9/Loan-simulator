5d3ea23687c3dacb51cc9e0870dcee493e7b52b0
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-09-07 20:17:27 -0300

    ssl

--- Backend/dist/config/database.js
+++ Backend/dist/config/database.js
@@ -9,12 +9,6 @@ module.exports = {
   username: "ghzxinjzpphaui",
   password: "218b80d1047e3827eed995aefeb5e09e31baf8c220b28313f1ae5de18b75bd6c",
   database: "db1dicruq3hqqe",
-  ssl: true,
-  dialectOptions: {
-    ssl: {
-      ca: 'path/to/ca'
-    },
-  },
   define: {
     timestamps: true,
     underscored: true,
